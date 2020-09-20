4d900d0aca1f2fed1c755155c82ab653fbaa2e85
mahdiyahafs <afifahmahdiyah29@gmail.com>
2020-09-20 21:02:11 +0700

    new activity

---
+++ app/src/main/java/com/example/helloworld/activity_mahdiyahafsr.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_mahdiyahafsr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mahdiyahafsr);
+    }
+}
